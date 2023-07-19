e1c03651f5188863697787cd50298ca66407d5cf
Mohamed Yasin <mohamedyasinkm16@gmail.com>
2023-07-19 19:01:15 +0530

    Final Commit

--- low_power.py
+++ low_power.py
@@ -5,6 +5,6 @@ from playsound import playsound
 run = True
 while run:
     power = psutil.sensors_battery()
-    if power.percent<=72 and power.power_plugged==False:
+    if power.percent<=20 and power.power_plugged==False:
         playsound("Binks Sake - Japanese ! Bgm ! Theme.mp3")
         run = False
