db5b4a48fe3793bdb31925953f480ebb53a01083
Mohamed Yasin <mohamedyasinkm16@gmail.com>
2023-07-19 18:58:28 +0530

    Final commit

--- low_power.py
+++ low_power.py
@@ -1,10 +1,10 @@
 #Importing required modules
 import psutil
-import playsound
+from playsound import playsound
 
 run = True
 while run:
     power = psutil.sensors_battery()
-    if power.percent<=20 and power.power_plugged==False:
-        playsound("E:\battery_charged_alert\Binks Sake - Japanese ! Bgm ! Theme.mp3")
+    if power.percent<=72 and power.power_plugged==False:
+        playsound("Binks Sake - Japanese ! Bgm ! Theme.mp3")
         run = False
